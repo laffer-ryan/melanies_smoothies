8dec0114b9c0e7fb0234ddc9e7308a77a4653f0c
Ryan Lafferty <90232580+laffer-ryan@users.noreply.github.com>
2026-03-25 07:03:26 -1000

    Change title from 'Customise Your Smoothie' to 'My Parents New Healthy Diner'

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
-st.title(f"Customise Your Smoothie")
+st.title(f"My Parents New Healthy Diner")
 st.write(
   """Choose the fruit you want in your custom smoothie
   """
